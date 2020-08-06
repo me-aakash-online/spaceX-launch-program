ccc04e2feb95252202068cef050eb554560dfde8
Aakash Jha <aakash_96@outlook.com>
2020-08-06 21:53:16 +0530

    Updates

---
+++ precache-manifest.550dba838a9257ac72758bc675e6d09b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70160bc49b6989f8242b37c244ba8b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16e5993b06cce3c3e363",
+    "url": "/static/css/main.36ea8270.chunk.css"
+  },
+  {
+    "revision": "d9219431cae67906179b",
+    "url": "/static/js/2.5defb1f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5defb1f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e5993b06cce3c3e363",
+    "url": "/static/js/main.dc384f5c.chunk.js"
+  },
+  {
+    "revision": "f209894845991e0ac97b",
+    "url": "/static/js/runtime-main.29d80761.js"
+  }
+]);
